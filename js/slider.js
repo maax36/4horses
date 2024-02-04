7ac9e75c7591b6168ef107aa16cd9844545da554
maax36 <freeivi8888@gmail.com>
2024-02-04 12:46:14 +0300

    update slider/css

--- js/slider.js
+++ js/slider.js
@@ -1,50 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  //update dom mobile
-  ////////////
-  var slidersDiv = document.querySelector('.sliders');
-  var isMobile = window.innerWidth <= 767;
-
-  if (isMobile) {
-      wrapBlocks(slidersDiv, ['b1', 'b2'], 'b', 'b1-b2');
-      wrapBlocks(slidersDiv, ['b3'], 'b3', 'b');
-      wrapBlocks(slidersDiv, ['b4', 'b5'], 'b', 'b4-b5');
-      wrapBlocks(slidersDiv, ['b6'], 'b6', 'b');
-      wrapBlocks(slidersDiv, ['b7'], 'b7', 'b');
-  }
-
-  function wrapBlocks(parentDiv, blockClasses, wrapperClass, addedClass) {
-      var blocksToWrap = Array.from(parentDiv.children).filter(function (child) {
-          return blockClasses.some(function (classPrefix) {
-              return child.classList.contains(classPrefix);
-          });
-      });
-
-      if (blocksToWrap.length >= 2) {
-          var wrapperDiv = document.createElement('div');
-          wrapperDiv.classList.add(wrapperClass, addedClass);
-
-          blocksToWrap.forEach(function (block) {
-              wrapperDiv.appendChild(block);
-          });
-
-          blocksToWrap.forEach(function (block) {
-              if (block.classList.contains('b')) {
-                  block.classList.remove('b');
-              }
-          });
-
-          var b3Index = Array.from(parentDiv.children).findIndex(function (child) {
-              return child.classList.contains('b3');
-          });
-
-          if (b3Index !== -1 && blockClasses.includes('b4')) {
-              parentDiv.insertBefore(wrapperDiv, parentDiv.children[b3Index + 1]);
-          } else {
-              parentDiv.insertBefore(wrapperDiv, parentDiv.firstChild);
-          }
-      }
-  }
-
+  
   //slider: mob - controls/Indicator
   ////////////
   let currentSlide = 0;
